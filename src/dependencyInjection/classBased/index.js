b05e880e82ef914f25490cbb3259967e547e45b3
sharanrprasad <s.prasad@crimsoneducation.org>
2020-04-13 00:46:43 +1200

    dependency injection examples

--- src/dependencyInjection/classBased/index.js
+++ src/dependencyInjection/classBased/index.js
@@ -1,10 +1,10 @@
 // Dependency injection is a form on inversion of control.
-// The main use case I found is initializing the third party services interaction like db needs a client object and this object can be dependency injected.
+// The main use case I found is initializing the third party services like db or api which needs a client object in each function.
+// This can be achieved in multiple ways. Example -1 Using the classes. Example -2 Using Modules. Both the approaches are functional in nature.
 
 const User = require('./User');
 const Booking = require('./Booking');
 
-// This can be achieved in multiple ways. Example -1 Using the classes.
 // Inject the dependency into all the objects here and export and use this object else where.
 const apiServiceFactory = (client) => {
   return {
